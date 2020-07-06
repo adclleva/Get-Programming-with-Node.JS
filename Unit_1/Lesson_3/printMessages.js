085b4f167de157e489c054230112d613a69d803d
adclleva <arvin.lleva@yahoo.com>
2020-07-05 18:26:28 -0700

    Completed Lesson 3 by creating a Node Module

--- Unit_1/Lesson_3/printMessages.js
+++ Unit_1/Lesson_3/printMessages.js
@@ -1,2 +1,5 @@
 const messageModule = require("../../Unit_0/Lesson_2/messages");
+const addNumModule = require("./exportFunction");
+const addNum = addNumModule.addNum;
 messageModule.messages.forEach((message) => console.log(message));
+console.log(addNum(1, 3));
---
+++ Unit_1/Lesson_3/recipe_connection/index.js
@@ -0,0 +1,3 @@
+const cities = require("cities");
+var myCity = cities.zip_lookup("91913");
+console.log(myCity);
