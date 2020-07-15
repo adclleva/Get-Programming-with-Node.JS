0ad4fd1fead866ffc756187113f4c1dd07bfd72b
adclleva <arvin.lleva@yahoo.com>
2020-07-14 21:00:40 -0700

    Completed Lesson 8

---
+++ Unit_2/Lesson_8/first_express_project/main.js
@@ -0,0 +1,17 @@
+const port = 3000,
+  express = require("express"),
+  app = express();
+
+app
+  .get("/", (req, res) => {
+    res.send("Hello, Universe!");
+    console.log(req.params);
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
+  })
+  .listen(port, () => {
+    console.log(
+      `The Express.js server has started and is listening on port number ${port}`
+    );
+  });
